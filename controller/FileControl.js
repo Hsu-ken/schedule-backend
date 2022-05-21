4a7694141033fc610188e469e279dd74bc8a3191
Hsu-ken <qwe73320@gmail.com>
2022-05-22 05:31:06 +0800

    add different schedule and calendar function

--- controller/FileControl.js
+++ controller/FileControl.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const path = require('path');
 const FileControl = {
-    async write(schedule) {
+    async writecalendarlist(schedule) {
 
-        let rawdata = await JSON.parse(fs.readFileSync(path.join(__dirname + '/../localfile/schedule.json')));
+        let rawdata = await JSON.parse(fs.readFileSync(path.join(__dirname + '/../localfile/calendarlist.json')));
+
         // let cars = [{
         //         Name: "chevrolet chevelle malibu",
         //         Miles_per_Gallon: 18,
@@ -33,14 +34,59 @@ const FileControl = {
 
         let data = JSON.stringify(rawdata);
         //__dirname 
-        fs.writeFileSync(path.join(__dirname + '/../localfile/schedule.json'), data);
+        fs.writeFileSync(path.join(__dirname + '/../localfile/calendarlist.json'), data);
 
         return true;
 
     },
-    async read() {
+    async readcalendarlist() {
         // let data = await RecordStatusModel.findAll({});
-        let rawdata = fs.readFileSync(path.join(__dirname + '/../localfile/schedule.json'));
+        let rawdata = fs.readFileSync(path.join(__dirname + '/../localfile/calendarlist.json'));
+
+        let data = JSON.parse(rawdata);
+        console.log(data);
+        return data;
+    },
+    async writeschedule(schedule) {
+
+        let rawdata = await JSON.parse(fs.readFileSync(path.join(__dirname + '/../localfile/calendarlist.json')));
+        // let cars = [{
+        //         Name: "chevrolet chevelle malibu",
+        //         Miles_per_Gallon: 18,
+        //         Cylinders: 8,
+        //         Displacement: 307,
+        //         Horsepower: 130,
+        //         Weight_in_lbs: 3504,
+        //         Acceleration: 12,
+        //         Year: "1970-01-01",
+        //         Origin: "USA"
+        //     },
+        //     {
+        //         Name: "buick skylark 320",
+        //         Miles_per_Gallon: 15,
+        //         Cylinders: 8,
+        //         Displacement: 350,
+        //         Horsepower: 165,
+        //         Weight_in_lbs: 3693,
+        //         Acceleration: 11.5,
+        //         Year: "1970-01-01",
+        //         Origin: "USA"
+        //     }
+        // ];
+        schedule.forEach(data => {
+            rawdata.push(data)
+        });
+
+        let data = JSON.stringify(rawdata);
+        //__dirname 
+        fs.writeFileSync(path.join(__dirname + '/../localfile/schedulelist.json'), data);
+
+        return true;
+
+    },
+    async readschedule() {
+        // let data = await RecordStatusModel.findAll({});
+        let rawdata = fs.readFileSync(path.join(__dirname + '/../localfile/schedulelist.json'));
 
         let data = JSON.parse(rawdata);
         console.log(data);
--- routes/file.js
+++ routes/file.js
@@ -1,18 +1,36 @@
 const express = require("express");
 const File = express.Router({ mergeParams: true });
 const FileControl = require("../controller/FileControl")
-File.post("/write", async function(req, res) {
+File.post("/writecalendarlist", async function(req, res) {
 
 
-    let data = await FileControl.write(req.body);
+    let data = await FileControl.writecalendarlist(req.body);
     if (data) {
         res.status(200).send();
     } else {
         res.status(404).send();
     }
 });
-File.get("/read", async function(req, res) {
-    let data = await FileControl.read();
+File.get("/readcalendarlist", async function(req, res) {
+    let data = await FileControl.readcalendarlist();
+    if (data) {
+        res.status(200).json(data);
+    } else {
+        res.status(404).send();
+    }
+});
+File.post("/writeschedule", async function(req, res) {
+
+
+    let data = await FileControl.writeschedule(req.body);
+    if (data) {
+        res.status(200).send();
+    } else {
+        res.status(404).send();
+    }
+});
+File.get("/readschedule", async function(req, res) {
+    let data = await FileControl.readschedule();
     if (data) {
         res.status(200).json(data);
     } else {
